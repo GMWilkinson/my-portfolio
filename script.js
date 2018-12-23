0b13d874f41ae8175203c6bea41851fd23fcd1e6
Grant Wilkinson <wilkinsonmgrant@gmail.com>
2018-12-23 13:08:09 +0000

    added modal functions

---
+++ script.js
@@ -0,0 +1,17 @@
+
+// const modal1 = document.querySelector('.modal1');
+// const modal2 = document.querySelector('.modal2');
+// const modal3 = document.querySelector('.modal3');
+// const modal4 = document.querySelector('.modal4');
+// const modal5 = document.querySelector('.modal5');
+// const makebookMobile = document.getElementById('makebookMobile').addEventListener('click', modalFunction);
+// const makebookDesktop = document.getElementById('makebookDesktop').addEventListener('click', modalFunction);
+// const events = document.getElementById('events').addEventListener('click', modalFunction);
+// const mangaBook = document.getElementById('mangaBook').addEventListener('click', modalFunction);
+// const codebusters = document.getElementById('codebusters').addEventListener('click', modalFunction);
+// const modals = [modal1, modal2, modal3, modal4, modal5];
+// document.getElementById('modalClose').addEventListener('click', modalCloseFunction);
+//
+// function modalFunction() {
+//   return modals[1].classList.toggle('is-active');
+// }
