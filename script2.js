3edcf5d541cf8b757d16885f2522b65444868fd3
Grant Wilkinson <wilkinsonmgrant@gmail.com>
2018-12-24 14:54:38 +0000

    added some styling

--- script2.js
+++ script2.js
@@ -1,25 +1,20 @@
 // Open the Modal
-function openModal() {
+const modal = document.querySelectorAll('.modal');
+function openModal1() {
   return document.getElementById('myModal1').style.display = 'block';
 }
 function openModal2() {
   return document.getElementById('myModal2').style.display = 'block';
 }
-// Close the Modal
-function closeModal() {
-  document.getElementById('myModal1').style.display = 'none';
+function openModal3() {
+  return document.getElementById('myModal3').style.display = 'block';
 }
-function closeModal2() {
-  document.getElementById('myModal2').style.display = 'none';
+function openModal4() {
+  return document.getElementById('myModal4').style.display = 'block';
 }
 
-var slideIndex = 1;
+let slideIndex = 1;
 showSlides(slideIndex);
-
-// Next/previous controls
-function plusSlides(n) {
-  showSlides(slideIndex += n);
-}
 
 // Thumbnail image controls
 function currentSlide(n) {
@@ -27,10 +22,13 @@ function currentSlide(n) {
 }
 
 function showSlides(n) {
-  var i;
-  var slides = document.getElementsByClassName('mySlides');
-  var dots = document.getElementsByClassName('demo');
-  var captionText = document.getElementById('caption');
+  let i;
+  const slides = document.getElementsByClassName('mySlides');
+  const dots = document.getElementsByClassName('demo');
+  const captionText1 = document.getElementById('caption1');
+  const captionText2 = document.getElementById('caption2');
+  const captionText3 = document.getElementById('caption3');
+  const captionText4 = document.getElementById('caption4');
   if (n > slides.length) {
     slideIndex = 1;
   }
@@ -45,5 +43,20 @@ function showSlides(n) {
   }
   slides[slideIndex-1].style.display = 'block';
   dots[slideIndex-1].className += ' active';
-  captionText.innerHTML = dots[slideIndex-1].alt;
+  captionText1.innerHTML = dots[slideIndex-1].alt;
+  captionText2.innerHTML = dots[slideIndex-1].alt;
+  captionText3.innerHTML = dots[slideIndex-1].alt;
+  captionText4.innerHTML = dots[slideIndex-1].alt;
 }
+
+window.onclick = function(event) {
+  if (event.target === modal[0]) {
+    modal[0].style.display = 'none';
+  }  if (event.target === modal[1]) {
+    modal[1].style.display = 'none';
+  }  if (event.target === modal[2]) {
+    modal[2].style.display = 'none';
+  }  if (event.target === modal[3]) {
+    modal[3].style.display = 'none';
+  }
+};
