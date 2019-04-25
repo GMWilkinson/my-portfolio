72cd201e3b5776911115bd2807428439f8f69583
Grant Wilkinson <wilkinsonmgrant@gmail.com>
2019-04-25 17:14:35 +0100

    Updates

---
+++ precache-manifest.0bf2b06bc3b14fe0c20a5c27d345bb6b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5845895d39b87fd54625",
+    "url": "/static/css/main.e4f8ce5e.chunk.css"
+  },
+  {
+    "revision": "5845895d39b87fd54625",
+    "url": "/static/js/main.afc53e8f.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "0dec233259a367d4dad6",
+    "url": "/static/css/2.97a8d7cb.chunk.css"
+  },
+  {
+    "revision": "0dec233259a367d4dad6",
+    "url": "/static/js/2.b5ee036e.chunk.js"
+  },
+  {
+    "revision": "582fb5aba9795ca2542c11d8023409b1",
+    "url": "/index.html"
+  }
+];
