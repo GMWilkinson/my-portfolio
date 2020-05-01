fd70919dfbbc3a3620009bcba0212dabcf9d0a8c
Grant Wilkinson <grant@aimermedia.com>
2020-05-01 16:15:19 +0100

    Added carousel

---
+++ build/precache-manifest.a38fd3e220205334fb45af8a7edd3b41.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "35ddf56f72f4acef3e27",
+    "url": "/static/css/main.c7d4443e.chunk.css"
+  },
+  {
+    "revision": "35ddf56f72f4acef3e27",
+    "url": "/static/js/main.e0ba1226.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "085e69e82091bd048d40",
+    "url": "/static/css/2.97a8d7cb.chunk.css"
+  },
+  {
+    "revision": "085e69e82091bd048d40",
+    "url": "/static/js/2.d66ef01b.chunk.js"
+  },
+  {
+    "revision": "97c44dd53d4914ea566330cbe6e5240d",
+    "url": "/index.html"
+  }
+];
