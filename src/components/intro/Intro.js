66b9b59b797c07c66aa382d46aea03bbc2a08c99
Grant Wilkinson <grant@aimermedia.com>
2020-05-01 16:22:15 +0100

    Added carousel

---
+++ src/components/intro/Intro.js
@@ -0,0 +1,71 @@
+
+import React from "react";
+import Projects from '../projects/Projects';
+import TechLists from './TechLists';
+import MethLists from './MethLists';
+import Testing from './TestLists';
+import Frameworks from './FrameworksLists';
+
+import { Card, Row, Col, UncontrolledCollapse, CardBody, Button }  from 'reactstrap';
+
+class Intro extends React.Component {
+  constructor() {
+    super();
+    this.toggle = this.toggle.bind(this);
+    this.state = {
+      collapse: false,
+      image: ''
+     };
+  }
+
+
+  toggle() {
+    this.setState(
+      state => ({
+        collapse: !state.collapse
+      }));
+  }
+
+  slide() {
+    this.setState({
+      animate: 'slide-out',
+      newSection: 'slide-in-section'
+    })
+  }
+
+
+  render() {
+    return (
+      <articlse>
+        <div className="">
+          <h2 className="" id="about">About</h2>
+        </div>
+        <div>
+          <p>Thanks for taking the time to check out my portfolio.</p>
+          <p>I'm from London and have just finished my first commercial contract at TokenAnalyst,
+           I have also completed the Immersive Web Development course at GA.</p>
+          <p>I am currently focusing on ReactJS, node.js
+          and keeping my vanilla javascript up to date</p>
+          <p>Really enjoying coding and just wish I'd gotten into it sooner.</p>
+          <p>I'm currently looking for new opportunities as a Junior Developer.</p>
+          <Row>
+            <Col>
+              <TechLists />
+            </Col>
+            <Col>
+              <MethLists />
+            </Col>
+            <Col>
+              <Testing />
+            </Col>
+            <Col>
+              <Frameworks />
+            </Col>
+          </Row>
+        </div>
+      </article>
+    );
+  }
+}
+
+export default Intro;
---
+++ src/components/projects/ProjectIntro.js
@@ -0,0 +1,34 @@
+
+import React from "react";
+
+import { Row, Col, Collapse, CardBody, Button }  from 'reactstrap';
+
+class Intro extends React.Component {
+  constructor() {
+    super();
+    this.toggle = this.toggle.bind(this);
+    this.state = { collapse: false };
+  }
+
+  toggle() {
+    this.setState(state => ({ collapse: !state.collapse }));
+  }
+
+  render() {
+    return (
+      <CardBody>
+        <p>
+        These are the projects that I have made to date.
+        tokenanalyst.io is my most recent project.
+        I was given seven days to complete each of the GA projects,
+        I haven't done anything to them since so that you can see my progression.
+        Nodestats is the project that I am most proud of. It was the first time that I had worked
+        with huge amounts of data and google charts. I learned a so much while doing this project.
+          <p>I hope that you like them.</p>
+        </p>
+      </CardBody>
+    );
+  }
+}
+
+export default Intro;
